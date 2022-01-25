4625fbafb1c2ba3a8c938bbfc6036b4d7b348ce3
junior-x <marcosmansur@outlook.com>
2022-01-25 12:35:05 -0800

    css

---
+++ pages/app.js
@@ -0,0 +1,25 @@
+const GlobalStyle = () => {
+    return (
+      <style global jsx>{`
+        @import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;700&display=swap");
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          font-family: "IBM Plex Sans", sans-serif !important;
+        }
+        body {
+          min-height: 100vh;
+        }
+      `}</style>
+    );
+  };
+  
+  export default function MyApp({ Component, pageProps }) {
+    return (
+      <>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </>
+    );
+  }
---
+++ pages/index.js
@@ -0,0 +1,173 @@
+import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import React from "react";
+import appConfig from "../config.json";
+
+const Title = (props) => {
+  const Tag = props.tag;
+  return (
+    <>
+      <Tag>{props.children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: ${appConfig.theme.colors.neutrals["000"]};
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default function Home() {
+  const [username, setUsername] = React.useState("junior-x");
+  return (
+    <>
+      <Box
+        styleSheet={{
+          minHeight: "100vh",
+          padding: "20px 0",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          background:
+            "url(https://wallpaperforu.com/wp-content/uploads/2020/11/astronaut-wallpaper2011162120441-scaled.jpg) no-repeat center center / cover",
+        }}
+      >
+        <Box
+          styleSheet={{
+            fontWeight: "500",
+            width: "90%",
+            maxWidth: "700px",
+            textAlign: "center",
+            padding: "12px",
+            color: "white",
+            backgroundColor: `${appConfig.theme.colors.primary[500]}60`,
+            boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+            backdropFilter: "blur( 17px )",
+            borderRadius: " 5px 5px 0 0",
+            border: "1px solid rgba( 255, 255, 255, 0.18 )",
+            borderBottom: "0",
+          }}
+        >
+          Um chat para todos que desejam, como um foguete, decolar na
+          programação.
+        </Box>
+        <Box
+          styleSheet={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            flexDirection: {
+              xs: "column",
+              md: "row",
+            },
+            width: "90%",
+            maxWidth: "700px",
+            borderRadius: "0 0 5px 5px",
+            padding: "32px",
+            boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+            backgroundColor: `${appConfig.theme.colors.neutrals[700]}f1`,
+          }}
+        >
+          {/* Formulário */}
+          <Box
+            as="form"
+            styleSheet={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              width: { xs: "100%", sm: "50%" },
+              textAlign: "center",
+              marginBottom: "32px",
+            }}
+          >
+            <Title tag="h2">Boas vindas de volta!</Title>
+            <Text
+              variant="body3"
+              styleSheet={{
+                marginBottom: "32px",
+                color: appConfig.theme.colors.neutrals[250],
+              }}
+            >
+              {appConfig.name}
+            </Text>
+
+            <TextField
+              onChange={(event) => {
+                const value = event.target.value;
+                setUsername(value);
+              }}
+              fullWidth
+              textFieldColors={{
+                neutral: {
+                  textColor: appConfig.theme.colors.neutrals[200],
+                  mainColor: appConfig.theme.colors.neutrals[900],
+                  mainColorHighlight: appConfig.theme.colors.primary[500],
+                  backgroundColor: appConfig.theme.colors.neutrals[800],
+                },
+              }}
+            />
+            <Button
+              type="submit"
+              label="Entrar"
+              fullWidth
+              buttonColors={{
+                contrastColor: appConfig.theme.colors.neutrals["000"],
+                mainColor: appConfig.theme.colors.primary[550],
+                mainColorLight: appConfig.theme.colors.primary[400],
+                mainColorStrong: appConfig.theme.colors.primary[700],
+              }}
+            />
+          </Box>
+          {/* Formulário */}
+
+          {/* Photo Area */}
+          <Box
+            styleSheet={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              maxWidth: "200px",
+              padding: "16px",
+              backgroundColor: appConfig.theme.colors.neutrals[800],
+              border: "1px solid",
+              borderColor: appConfig.theme.colors.neutrals[999],
+              borderRadius: "10px",
+              flex: 1,
+              minHeight: "240px",
+            }}
+          >
+            <Image
+              styleSheet={{
+                borderRadius: "50%",
+                marginBottom: "16px",
+              }}
+              src={`https://github.com/${username}.png`}
+            />
+            <Text
+              tag="a"
+              href={`https://github.com/${username}`}
+              variant="body4"
+              styleSheet={{
+                textDecoration: "none",
+                color: appConfig.theme.colors.neutrals[200],
+                backgroundColor: appConfig.theme.colors.neutrals[900],
+                padding: "3px 10px",
+                borderRadius: "1000px",
+              }}
+              onMouseOver={(event) => {
+                event.target.style.textDecoration = "underline";
+              }}
+              onMouseLeave={(event) => {
+                event.target.style.textDecoration = "none";
+              }}
+            >
+              {username}
+            </Text>
+          </Box>
+          {/* Photo Area */}
+        </Box>
+      </Box>
+    </>
+  );
+}
